b705195ed1df13a2b207279b3357024c4606f833
Administrator <1021237228@qq.com>
2019-03-28 20:15:22 +0800

    添加了ImageUtils

--- app/src/main/java/com/zhowin/basicframework/common/utils/PhoneUtils.java
+++ app/src/main/java/com/zhowin/basicframework/common/utils/PhoneUtils.java
@@ -1,6 +1,17 @@
 package com.zhowin.basicframework.common.utils;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.RequiresPermission;
+import android.telephony.SmsManager;
 import android.text.TextUtils;
+
+import com.zhowin.basicframework.common.base.BaseApplication;
+
+import java.util.List;
+
+import static android.Manifest.permission.SEND_SMS;
 
 /**
  * author      : Z_B
@@ -49,7 +60,38 @@ public final class PhoneUtils {
         }
         return true;
     }
+    /**
+     * 打电话
+     */
+    public static void callPhone(final String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+        BaseApplication.getInstance().startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
 
+    /**
+     * 发信息
+     */
+    public static void sendSms(final String phoneNumber, final String content) {
+        Uri uri = Uri.parse("smsto:" + phoneNumber);
+        Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+        intent.putExtra("sms_body", content);
+        BaseApplication.getInstance().startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    @RequiresPermission(SEND_SMS)
+    public static void sendSmsSilent(final String phoneNumber, final String content) {
+        if (TextUtils.isEmpty(content)) return;
+        PendingIntent sentIntent = PendingIntent.getBroadcast(BaseApplication.getInstance(), 0, new Intent("send"), 0);
+        SmsManager smsManager = SmsManager.getDefault();
+        if (content.length() >= 70) {
+            List<String> ms = smsManager.divideMessage(content);
+            for (String str : ms) {
+                smsManager.sendTextMessage(phoneNumber, null, str, sentIntent, null);
+            }
+        } else {
+            smsManager.sendTextMessage(phoneNumber, null, content, sentIntent, null);
+        }
+    }
 
 
 }
